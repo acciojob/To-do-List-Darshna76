70bea29b0ee41be0b13504a2e02478eade2bd2a8
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-05-07 20:07:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-//your code here
 
+var btn = document.getElementById("addTodoBtn");
+var todolist = document.getElementById("todoList");
+btn.addEventListener("click", function() {
+	var todo = document.getElementById("newTodoInput").value;
+	
+  todolist.innerHTML += `<li>${todo}</li>`;
+});
